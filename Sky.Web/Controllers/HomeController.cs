16f404191d0dca9f7a93f6b1f494e84fe27c7207
sky <ismatch@qq.com>
2017-09-22 17:46:07 +0800

    添加项目文件。

---
+++ Sky.Web/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sky.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 文章专栏
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Article()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 文章详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 资源分享
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Resource()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 点点滴滴
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TimeLine()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 关于本站
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
